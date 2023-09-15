dfba51a3ac5c2da881413f9c4acc15489cd5073d
202310961 <giunnae77@naver.com>
2023-09-15 21:47:36 +0900

    Add C SourceFile [과제1.c]

---
+++ 과제1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1 = 10;
+	int num2 = 15;
+	printf("%d & %d = %d \n", num1, num2, num1&num2);
+	printf("%d | %d = %d \n", num1, num2, num1| num2);
+	printf("%d ^ %d = %d \n", num1, num2, num1^ num2);
+
+
+	return 0;
+
+}
